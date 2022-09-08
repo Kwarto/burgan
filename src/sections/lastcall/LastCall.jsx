644d1498182bffabf35ec76d4e8eaa1c9cd80d61
Kwarto <dbitsolu@gmail.com>
2022-09-08 11:21:29 +0000

    adding mobile responsiveness 2

--- src/sections/lastcall/LastCall.jsx
+++ src/sections/lastcall/LastCall.jsx
@@ -5,7 +5,7 @@ function LastCall() {
     <>
     <LastCallContainer>
      <LastWrapper className='container'>
-      <h1>Quickly And Easily Pay Client & Contractors</h1>
+      <h1>Quickly And Easily Pay Clients & Contractors</h1>
       <h4>Spend Less On Fees And More On Growth With Domestic And Digital Business Payments</h4>
       <p>
       Make A Payment To Suppliers, Contractors, And Partners Around The World From Your Burgan Account
